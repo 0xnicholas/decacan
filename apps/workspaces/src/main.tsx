79cf299f9a7476c801f060accc2df87c83e93a6d
nicholasli <nicholasli9@qq.com>
2026-04-02 14:01:41 +0800

    feat(workspaces): migrate to shared ReUI theme with dark mode support

--- apps/workspaces/src/main.tsx
+++ apps/workspaces/src/main.tsx
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@decacan/ui';
 
-import { App } from "./app/App";
-import "./css/styles.css";
+import { App } from './app/App';
+import './css/styles.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="system" storageKey="workspaces-theme">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
